149eea23dd191602fa39377cb6d14357378a1790
becolomochi <becolomochi@gmail.com>
2020-04-25 15:25:00 +0900

    日付の表示を変更

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,18 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
