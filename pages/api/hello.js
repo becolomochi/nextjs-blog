f049d28e95ad78a0a4c501c0014c14d5cd95a7b5
becolomochi <becolomochi@gmail.com>
2020-04-25 16:17:14 +0900

    POSTリクエストを処理するAPIを書く場合の書き方

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,4 +4,8 @@
 // res = response data
 export default (req, res) => {
   res.status(200).json({ text: 'Hello!'})
+
+  // Handle Form Input
+  // const email = req.body.email
+  // Then save email to your database, etc...
 }
