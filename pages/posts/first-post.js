7c1098d9d5806e5dfa977d272e0e0a733924520a
becolomochi <becolomochi@gmail.com>
2020-04-25 10:41:17 +0900

    Linkコンポーネントの作成

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,16 @@
-// default 必要。
+import Link from 'next/link'
+
 export default function FirstPost() {
-  return <h1>First Post</h1>
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          {/* リンク要素にhrefつける */}
+          <a>Back to home</a>
+          {/* a要素には属性をつけない */}
+        </Link>
+      </h2>
+    </>
+  )
 }
