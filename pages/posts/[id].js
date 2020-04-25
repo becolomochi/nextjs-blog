ad50bf05458421453908e26949af76c1b660fc36
becolomochi <becolomochi@gmail.com>
2020-04-25 16:48:48 +0900

    Dateコンポーネントの呼び出し忘れを追加

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Date from '../../components/date'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.scss'
